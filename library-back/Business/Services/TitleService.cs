f9c3e12f7052efd3ff5689a253c9a1f5308fba20
Geovanni Rojas <geovanny118@hotmail.com>
2023-07-06 14:31:26 -0500

    Se crea los servicios para titulo

---
+++ library-back/Business/Services/TitleService.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Library.Business.Interfaces;
+using Library.Infrastructure.Models;
+using Library.Infrastructure.Interfaces;
+
+namespace Library.Business.Services;
+
+public class TitleService : ITitleService 
+{
+    private readonly ITitleRepository _repo;
+
+    public TitleService(ITitleRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<IEnumerable<Title>> GetAllAsync()
+    {
+        return await _repo.GetAllAsync();
+    }
+
+    public async Task<Title> GetByIdAsync(int id)
+    {
+        return await _repo.GetByIdAsync(id);    
+    }
+
+    public async Task AddAsync(Title entity)
+    {
+        await _repo.AddAsync(entity);
+        await _repo.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Title entity)
+    {
+        _repo.Update(entity);
+        await _repo.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Title entity)
+    {
+        _repo.Delete(entity);
+        await _repo.SaveChangesAsync();
+    }
+}
