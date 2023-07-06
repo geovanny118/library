952b01c60cccab339c4585540182cbfb82c9f24b
Geovanni Rojas <geovanny118@hotmail.com>
2023-07-06 12:48:08 -0500

    Se crea los servicios para autores

---
+++ library-back/Business/Interfaces/IAuthorService.cs
@@ -0,0 +1,8 @@
+using Library.Infrastructure.Models;
+
+namespace Library.Business.Interfaces;
+
+public interface IAuthorService : IService<Author>
+{
+    
+}
---
+++ library-back/Business/Services/AuthorService.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Library.Business.Interfaces;
+using Library.Infrastructure.Models;
+using Library.Infrastructure.Interfaces;
+
+namespace Library.Business.Services;
+
+public class AuthorService : IAuthorService
+{
+    private readonly IAuthorRepository _repo;
+
+    public AuthorService(IAuthorRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<IEnumerable<Author>> GetAllAsync()
+    {
+        return await _repo.GetAllAsync();
+    }
+
+    public async Task<Author> GetByIdAsync(int id)
+    {
+        return await _repo.GetByIdAsync(id);
+    }
+
+    public async Task AddAsync(Author entity)
+    {
+        await _repo.AddAsync(entity);
+        await _repor.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Author entity)
+    {
+        _repo.Update(entity);
+        await _repo.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Author entity)
+    {
+        _repo.Delete(entity);
+        await _repo.SaveChangesAsync();
+    }
+}
