f0c831ea86279b393c1658569a4932b2f301c90d
Geovanni Rojas <geovanny118@hotmail.com>
2023-07-06 14:09:49 -0500

    Se configura el repositorio para titulos

--- library-back/Infrastructure/Models/Title.cs
+++ library-back/Infrastructure/Models/Title.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Text.Json.Serialization;
 namespace Library.Infrastructure.Models;
 
 public partial class Title
@@ -9,9 +9,12 @@ public partial class Title
 
     public string Title1 { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual ICollection<AuthorsTitle> AuthorsTitles { get; set; } = new List<AuthorsTitle>();
 
+    [JsonIgnore]
     public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 
+    [JsonIgnore]
     public virtual ICollection<TitlesGender> TitlesGenders { get; set; } = new List<TitlesGender>();
 }
