b07fd71de2adc8907e58bd5e3f84b4d4dfbfb602
Geovanni Rojas <geovanny118@hotmail.com>
2023-07-14 11:01:58 -0500

    Se ajusta los endpoints para usuarios

--- library-back/Presentation/Controllers/UserController.cs
+++ library-back/Presentation/Controllers/UserController.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Library.Business.Interfaces;
+using Library.Infrastructure.Dtos;
 using Library.Infrastructure.Models;
 
 namespace Presentation.Controllers;
@@ -9,43 +11,51 @@ namespace Presentation.Controllers;
 public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
+    private readonly IMapper _mapper;
 
-    public UserController(IUserService userService)
+    public UserController(IUserService userService, IMapper mapper)
     {
         _userService = userService;
+        _mapper = mapper;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        return Ok(await _userService.GetAllAsync());
+        IEnumerable<User> users = await _userService.GetAllAsync();
+        var usersDto = _mapper.Map<IEnumerable<UserResponseDto>>(users);
+        return Ok(usersDto);
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
-        var entity = await _userService.GetByIdAsync(id);
-        return entity is null ? NotFound() : Ok(entity);
+        User entity = await _userService.GetByIdAsync(id);
+        var userDto = _mapper.Map<UserResponseDto>(entity);
+        return userDto is null ? NotFound() : Ok(userDto);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] User user)
+    public async Task<IActionResult> Create([FromBody] UserCreateDto userDto)
     {
-        await _userService.AddAsync(user);
+        User userToCreate = _mapper.Map<User>(userDto);
+        await _userService.AddAsync(userToCreate);
         return Ok();
     }
 
     [HttpPut]
-    public async Task<IActionResult> Update([FromBody] User user)
+    public async Task<IActionResult> Update([FromBody] UserUpdateDto userDto)
     {
-        await _userService.UpdateAsync(user);
+        User userToUpdate = await _userService.GetByIdAsync(userDto.Id);
+        _mapper.Map(userDto, userToUpdate);
+        await _userService.UpdateAsync(userToUpdate);
         return Ok();
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(int id)
     {
-        var entity = await _userService.GetByIdAsync(id);
+        User entity = await _userService.GetByIdAsync(id);
         await _userService.DeleteAsync(entity);
         return Ok();
     }
